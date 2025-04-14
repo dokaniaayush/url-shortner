196ba022ceec801bd22c87012f9747ddf86c3345
Ayush Kumar Dokania <ayush.dokania@convegenius.ai>
2025-04-14 20:29:56 +0530

    url shortner

---
+++ service/auth.js
@@ -0,0 +1,34 @@
+// for stateLess
+// const sessionIdToUserMap = new Map(); 
+
+// function setUser(id, user) {
+//   sessionIdToUserMap.set(id, user);
+// }
+
+// function getUser(id) {
+//   return sessionIdToUserMap.get(id);
+// }
+
+// for jwt token
+const jwt = require("jsonwebtoken");
+const secret = "Ayush$$2001$$";
+
+function setUser(user) {
+  return jwt.sign(
+    {
+      _id: user._id,
+      email: user.email
+    }, 
+  secret);
+}
+
+function getUser(token) {
+  if(!token) return null;
+  try {
+    return jwt.verify(token, secret);
+  } catch (error) {
+    return null;
+  }
+}
+
+module.exports = { setUser, getUser };
